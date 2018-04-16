ae47050a52d8578d2802ee574815e828b672f7b5
Roberto Röseling Badô <1571329+robertobado@users.noreply.github.com>
2018-04-16 13:17:10 -0300

    echoing complete dict

--- vars/buildViaJervis.groovy
+++ vars/buildViaJervis.groovy
@@ -70,6 +70,7 @@ List getJervisMetaData(String project, String JERVIS_BRANCH) {
         throw new FileNotFoundException('Cannot find .jervis.yml nor .travis.yml')
     }
    def jervis_dict = new Yaml().load(jervis_yaml);
+   echo "${jervis_dict}"
    if('jervis' in jervis_dict.keySet()){
       def jervis_map = jervis_yaml['jervis']
       echo "${jervis_map}"
