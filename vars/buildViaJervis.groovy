4fb33e184407f3785398177268b392d43f73bd62
Roberto Röseling Badô <1571329+robertobado@users.noreply.github.com>
2018-04-16 19:47:39 -0300

    fixing if format

--- vars/buildViaJervis.groovy
+++ vars/buildViaJervis.groovy
@@ -200,7 +200,7 @@ def call() {
     echo "in proccess map=${jervis_yamls}"
     for(String component_name : jervis_yamls.keySet()) {
        if ((componentOnly.empty || component_name in componentOnly)
-           && (componentExcept.empty || component_name not in componentExcept) ) {
+           && (componentExcept.empty || ! component_name in componentExcept) ) {
               jervis_tasks[component_name] = {
                  stage("Forking to ${component_name}") {
                      buildViaJervis(jervis_yamls[component_name],folder_listing)
