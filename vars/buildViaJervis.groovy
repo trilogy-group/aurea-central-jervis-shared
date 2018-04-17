7c0a1d6e1406b20be229f8ad254fba384dfa7097
Roberto Röseling Badô <1571329+robertobado@users.noreply.github.com>
2018-04-17 03:12:24 -0300

    Update buildViaJervis.groovy

--- vars/buildViaJervis.groovy
+++ vars/buildViaJervis.groovy
@@ -347,7 +347,7 @@ def buildViaJervis(String jervis_yaml, List folder_listing, String component_nam
                   changeset -> changeset.each{ 
                      change -> echo change.comment
                      if(change.comment.contains('[ci ')) {
-                        def ci_hint_list = change.comment.contains.split('[ci ')[1].split(']')[0].split(' ')
+                        def ci_hint_list = change.comment.split('[ci ')[1].split(']')[0].split(' ')
                         for (ci_hint in ci_hint_list){
                            echo "ci_hint=${ci_hint}"
                            switch (ci_hint) {
