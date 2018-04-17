91a95d9058388fe13d4c0e8edef84dd9d74de7d9
Roberto Röseling Badô <1571329+robertobado@users.noreply.github.com>
2018-04-17 10:28:32 -0300

    Update buildViaJervis.groovy

--- vars/buildViaJervis.groovy
+++ vars/buildViaJervis.groovy
@@ -346,6 +346,23 @@ def buildViaJervis(String jervis_yaml, List folder_listing, String component_nam
                 List componentOnly = []
                 List componentExcept = []
                echo "currentBuild=" + currentBuild.dump()
+               
+               def changeLogSets = currentBuild.rawBuild.changeSets
+               for (int i = 0; i < changeLogSets.size(); i++) {
+                   def entries = changeLogSets[i].items
+                   for (int j = 0; j < entries.length; j++) {
+                       def entry = entries[j]
+                       echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+                       def files = new ArrayList(entry.affectedFiles)
+                       for (int k = 0; k < files.size(); k++) {
+                           def file = files[k]
+                           echo "  ${file.editType.name} ${file.path}"
+                       }
+                   }
+}
+               
+               
+               
                 currentBuild.changeSets.each{ 
                     changeset -> changeset.each{ 
                     change -> 
